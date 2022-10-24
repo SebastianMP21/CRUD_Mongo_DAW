e27e838fd8b2ffcaca824cf86cf1c8932638dcf1
SebastianMP21 <sebastian.marquez@tecsup.edu.pe>
2022-10-23 22:17:13 -0500

    lab7

---
+++ src/components/test-component/controller.js
@@ -0,0 +1,36 @@
+import Student from "../../models/student";
+
+export const findAll = async (req, res) => {
+ try {
+   const students = await Student.find();
+
+   res.json({
+     ok: true,
+     data: students,
+   });
+ } catch (error) {
+   res.json({
+     ok: false,
+     data: error.message,
+   });
+ }
+};
+
+export const create = async (req, res) => {
+ try {
+   const { body } = req;
+   const user = new Student(body);
+   user.save();
+
+   res.json({
+     ok: true,
+     data: user,
+   });
+ } catch (error) {
+   res.json({
+     ok: false,
+     data: error.message,
+   });
+ }
+};
+
